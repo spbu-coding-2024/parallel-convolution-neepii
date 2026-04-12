c697632bec45f18aa9fed1c622c7dd18f54f7316
Nikita Samodurov <nikita98351@gmail.com>
2026-04-12 11:10:19 +0300

    feat: dynamic size of kernel matrices

--- src/conv.h
+++ src/conv.h
@@ -1,14 +1,19 @@
 #pragma once
 #include "cbmp.h"
+#include <stddef.h>
 #include <stdint.h>
 
 typedef struct {
-  int32_t mtx[3][3][3];
+  int32_t *mtx[3];
   int32_t denominator_coef;
+  size_t size;
 } KernelMatrix;
 
 void conv_apply_kernel_sequentialy(BMP *image, KernelMatrix kernel);
 
 KernelMatrix ker_identity();
-KernelMatrix ker_gauss_blur();
+KernelMatrix ker_3x3_gauss_blur();
+KernelMatrix ker_5x5_gauss_blur();
 KernelMatrix ker_ridge();
+
+void free_kernel_matrix(KernelMatrix ker);
