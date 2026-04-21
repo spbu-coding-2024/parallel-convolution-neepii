f885c65cf40dfd107649876b9b5b3b86e5972464
Nikita Samodurov <nikita98351@gmail.com>
2026-04-21 23:49:38 +0300

    ci: add tests

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,2 +1,2 @@
 find src -name "*.c" ! -path '*/lib/cbmp.c' | \
-          xargs clang-tidy -checks='-*,readability-*,performance-*' --warnings-as-errors
+          xargs clang-tidy -p "build/" -checks='-*,readability-*,performance-*' --warnings-as-errors
